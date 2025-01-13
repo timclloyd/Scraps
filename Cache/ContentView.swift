8da8f8e952591b43e96c229337b9561bec06e69a
Tim Lloyd <git@timlloyd.me>
2025-01-13 17:29:49 +0000

    Fixed scrolling-while-typing bug by removing WIP customisations

--- Cache/ContentView.swift
+++ Cache/ContentView.swift
@@ -14,7 +14,7 @@ struct ContentView: View {
     
     var textSize: CGFloat = 16
     var horizontalPadding: CGFloat = 16
-    var verticalPadding: CGFloat = 16
+    var verticalPadding: CGFloat = 0
     
     var body: some View {
         VStack(spacing: 0) {
@@ -32,7 +32,7 @@ struct ContentView: View {
             .onAppear {
                 isFocused = true // Focus cursor at the end when the view appears
             }
-            .onChange(of: currentText) { newValue in
+            .onChange(of: currentText) { oldValue, newValue in
                 // Auto-save when the text changes
                 if !newValue.isEmpty {
                     document.addLine(newValue)
@@ -46,7 +46,7 @@ struct TextEditorView: UIViewRepresentable {
     @Binding var text: String
     var font: UIFont
     var padding: EdgeInsets
-    
+
     func makeUIView(context: Context) -> UITextView {
         let textView = UITextView()
         textView.isScrollEnabled = true
@@ -61,41 +61,28 @@ struct TextEditorView: UIViewRepresentable {
             bottom: padding.bottom,
             right: padding.trailing
         )
-        
-        DispatchQueue.main.async {
-            let endPosition = textView.endOfDocument
-            textView.scrollRangeToVisible(NSRange(location: textView.offset(from: textView.beginningOfDocument, to: endPosition), length: 0))
-        }
-        
         return textView
     }
-    
+
     func updateUIView(_ uiView: UITextView, context: Context) {
         if uiView.text != text {
             uiView.text = text
         }
-        let endPosition = uiView.endOfDocument
-        uiView.scrollRangeToVisible(NSRange(location: uiView.offset(from: uiView.beginningOfDocument, to: endPosition), length: 0))
     }
-    
+
     func makeCoordinator() -> Coordinator {
         Coordinator(self)
     }
-    
+
     class Coordinator: NSObject, UITextViewDelegate {
         var parent: TextEditorView
-        
+
         init(_ parent: TextEditorView) {
             self.parent = parent
         }
-        
+
         func textViewDidChange(_ textView: UITextView) {
             parent.text = textView.text
-        }
-        
-        func textViewDidBeginEditing(_ textView: UITextView) {
-            let endPosition = textView.endOfDocument
-            textView.selectedTextRange = textView.textRange(from: endPosition, to: endPosition)
         }
     }
 }
