f142c391853610829cb897471bc96de918225abb
Tim Lloyd <git@timlloyd.me>
2026-04-29 14:29:48 +0100

    Sentiment widget reloads properly now

--- Cache/Managers/WidgetReloadScheduler.swift
+++ Cache/Managers/WidgetReloadScheduler.swift
@@ -3,18 +3,29 @@ import WidgetKit
 
 @MainActor
 final class WidgetReloadScheduler {
+    private static let widgetKinds = [
+        "LatestScrapWidget",
+        "HighlightSentimentWidget"
+    ]
+
     private var workItem: DispatchWorkItem?
 
     func scheduleReload() {
         workItem?.cancel()
         let workItem = DispatchWorkItem {
-            WidgetCenter.shared.reloadTimelines(ofKind: "LatestScrapWidget")
+            Self.reloadAllDataBackedWidgets()
         }
         self.workItem = workItem
         DispatchQueue.main.asyncAfter(deadline: .now() + 2, execute: workItem)
     }
 
     func reloadImmediately() {
-        WidgetCenter.shared.reloadTimelines(ofKind: "LatestScrapWidget")
+        Self.reloadAllDataBackedWidgets()
+    }
+
+    private static func reloadAllDataBackedWidgets() {
+        for kind in widgetKinds {
+            WidgetCenter.shared.reloadTimelines(ofKind: kind)
+        }
     }
 }
